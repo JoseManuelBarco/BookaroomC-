3d2786ac791687e3f9c50dcd8335986626ba2968
CEP-TARDA <jbarcoc2324@politecnics.tarda>
2025-03-31 17:23:31 +0200

    Prueba de la fuente

--- Bookaroom/EditReservationForm.Designer.cs
+++ Bookaroom/EditReservationForm.Designer.cs
@@ -57,9 +57,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(223)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Outfit", 9.749999F);
             this.label3.Location = new System.Drawing.Point(36, 293);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.Size = new System.Drawing.Size(39, 16);
             this.label3.TabIndex = 74;
             this.label3.Text = "Estat";
             // 
@@ -91,9 +92,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(223)))), ((int)(((byte)(192)))));
-            this.label1.Location = new System.Drawing.Point(37, 98);
+            this.label1.Font = new System.Drawing.Font("Outfit", 9.749999F);
+            this.label1.Location = new System.Drawing.Point(38, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.Size = new System.Drawing.Size(45, 16);
             this.label1.TabIndex = 70;
             this.label1.Text = "Usuari";
             // 
@@ -101,10 +103,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(195)))), ((int)(((byte)(150)))));
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
-            this.label5.Location = new System.Drawing.Point(263, 42);
+            this.label5.Font = new System.Drawing.Font("Outfit", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(232, 38);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(154, 20);
+            this.label5.Size = new System.Drawing.Size(203, 30);
             this.label5.TabIndex = 69;
             this.label5.Text = "EDITAR RESERVA";
             // 
@@ -135,9 +137,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(223)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Outfit", 9.749999F);
             this.label4.Location = new System.Drawing.Point(37, 219);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.Size = new System.Drawing.Size(90, 16);
             this.label4.TabIndex = 65;
             this.label4.Text = "Esdeveniment";
             // 
@@ -145,9 +148,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(223)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Outfit", 9.749999F);
             this.label2.Location = new System.Drawing.Point(37, 157);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.Size = new System.Drawing.Size(49, 16);
             this.label2.TabIndex = 64;
             this.label2.Text = "Butaca";
             // 
